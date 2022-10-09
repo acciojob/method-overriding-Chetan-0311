7575cd75274a1f1ce14b36a3d1f3b53c7473d439
Chetan-0311 <cjangra740@gmail.com>
2022-10-09 15:47:29 +0530

    Method over loading

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+  static class A{
+      public static String meth(){
+          return "Invoking method from class A";
+      }
+  }
+  static class B extends A{
+      public static String meth(){
+          return "Method is overridden in Extendend class B";
+      }
+  }
+
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.meth());
+    }
 }
